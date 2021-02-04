2632ca36abde9a7f8491cc773244f75e80ca5389
haoxiaopeng150 <2089299779@qq.com>
2021-02-04 09:05:10 +0800

    在IDEA创建本地库添加文件

---
+++ HelloGit/src/main/java/com/atguigu/Hellogit.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author haoxiaopeng
+ */
+public class Hellogit {
+    public  static  void main(String[] args){
+      System.out.println("Hello Git!");
+
+    }
+}
